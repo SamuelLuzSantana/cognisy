89e3054c1eab29e678d3a4be1ce4f3722d679f43
Samuel Luz Santana <samuelluzsantana@outlook.com>
2023-08-20 16:55:08 -0300

    project inir

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboardpage() {
+  return (
+    <>
+      <p> Dashboard Page (Protected)</p>
+    </>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+ 
+export default function LandingPage() {
+  return (
+    <>
+      <div>
+        Landing Page (Unprotected)
+      </div>
+    </>
+  )
+}
